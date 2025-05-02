546dbc6cda2c08c4618d76d1beae7c805d321602
keikhan <kayhan10433343@gmail.com>
2025-05-03 00:08:44 +0500

    Update build script with explicit migrations

--- build.sh
+++ build.sh
@@ -2,12 +2,19 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py makemigrations
-python manage.py migrate
+# Run database migrations
+python manage.py migrate auth
+python manage.py migrate admin
+python manage.py migrate contenttypes
+python manage.py migrate sessions
+python manage.py migrate users
+python manage.py migrate tasks
+python manage.py migrate home
+python manage.py migrate community
 
 # Collect static files
 python manage.py collectstatic --no-input 
